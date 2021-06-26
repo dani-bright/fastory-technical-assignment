0003a1c31cbb48c6d5081ab3c9756d5332b7fb3a
dani <beblidaniel@gmail.com>
2021-06-26 15:24:09 +0200

    add loader percentage

--- src/App.tsx
+++ src/App.tsx
@@ -38,27 +38,34 @@ export interface IAppProps {
 const App : FC<IAppProps> = (props) => {
     const { setVehicles, setPeoples, setFilms, setPlanets, setSpecies, setStarships } = props;
     const [showLoader, setShowLoader] = useState(true);
+    const [percentage, setPercentage] = useState(0);
     useEffect(() => {
         (async () => {
             const peoples = await axios.get('http://localhost:3001/people');
             setPeoples(peoples.data);
+            setPercentage(17)
             const films = await axios.get('http://localhost:3001/film');
             setFilms(films.data);
+            setPercentage(34)
             const vehicles = await axios.get('http://localhost:3001/vehicle');
             setVehicles(vehicles.data);
+            setPercentage(51)
             const species = await axios.get('http://localhost:3001/specie');
             setSpecies(species.data);
+            setPercentage(68)
             const starships = await axios.get('http://localhost:3001/starship');
             setStarships(starships.data);
+            setPercentage(85)
             const planets = await axios.get('http://localhost:3001/planet');
             setPlanets(planets.data);
+            setPercentage(100)
             setShowLoader(false);
         })()
     }, [setFilms, setVehicles, setPeoples, setSpecies, setStarships, setPlanets]);
     return (
         <HashRouter>
             <PopupProvider>
-                { showLoader ? <Loader/> : null }
+                { showLoader ? <Loader percentage={ percentage }/> : null }
                 <div className="menu">
                     <Link to="/">people</Link>
                     <Link to="/vehicules">vehicules</Link>
--- src/component/Loader.tsx
+++ src/component/Loader.tsx
@@ -2,10 +2,15 @@ import * as React from 'react';
 import { FC, ReactElement } from 'react';
 import spinner from '../asset/spinner.gif';
 
-export const Loader : FC = () : ReactElement => {
+export interface ILoaderProps {
+    percentage : number
+}
+
+export const Loader : FC<ILoaderProps> = ({ percentage }) : ReactElement => {
     return (
         <div className="loader">
             <img src={ spinner } alt="spinner"/>
+            <p>{ percentage }%</p>
         </div>
     )
 };
